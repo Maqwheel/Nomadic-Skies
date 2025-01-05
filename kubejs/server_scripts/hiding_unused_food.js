4ffc914c43de904b086be12a124c9151be1b1e20
Matthew McDermott <matthewmcd07@gmail.com>
2025-01-06 12:12:06 +1300

    Ube has been removed
    Can just add to list for other uncraftable food items
    
    closes #34

--- kubejs/server_scripts/hiding_unused_food.js
+++ kubejs/server_scripts/hiding_unused_food.js
@@ -1,6 +1,21 @@
 ServerEvents.recipes(
     event => {
-        event.remove({output: 'bamboodelight:bamboo_cake'});
-        event.remove({output: 'bamboodelight:piece_bamboo'});
+        const foods =  [
+            'createfood:raw_ube_cake_base',
+            'createfood:raw_ube_cookie',
+            'createfood:ube_sugar_dough',
+            'createfood:ube_cake_base',
+            'createfood:ube_cream_frosted_ube_cake',
+            'createfood:ube_cream_frosting',
+            'createfood:ube_cream_frosting_bottle',
+            'createfood:ube_cream_frosting_bucket'
+        ]
+
+        foods.forEach(
+            food => {
+                event.remove({input: food});
+                event.remove({output: food});
+            }
+        )
     }
 )
