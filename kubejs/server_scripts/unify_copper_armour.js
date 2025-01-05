0dffe0e5ac95380e09d99cf80a77b8146823ed14
Matthew McDermott <matthewmcd07@gmail.com>
2025-01-05 14:11:27 +1300

    Removed unneeded KubeJS scripts

--- kubejs/server_scripts/unify_copper_armour.js
+++
@@ -1,9 +0,0 @@
-// Removing duplicate copper armour
-ServerEvents.recipes(
-    event => {
-        event.remove({output: 'create_sa:copper_helmet'})
-        event.remove({output: 'create_sa:copper_chestplate'})
-        event.remove({output: 'create_sa:copper_leggings'})
-        event.remove({output: 'create_sa:copper_boots'})
-    }
-)
