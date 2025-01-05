b7331f711eaf8e53e6857155bf944c73ea996ae4
Matthew McDermott <matthewmcd07@gmail.com>
2025-01-05 14:12:28 +1300

    Delete remove_brass_ore.js

--- kubejs/startup_scripts/remove_brass_ore.js
+++
@@ -1,9 +0,0 @@
-WorldgenEvents.remove(
-    event => {
-        event.removeOres(
-            ores => {
-                ores.blocks = ['nebulus_desert_trader:brass']
-            }
-        )
-    }
-)
