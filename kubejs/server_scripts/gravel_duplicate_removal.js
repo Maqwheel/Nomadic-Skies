60f25e13deed58393fcc20acbe92867aa6c9494a
Matthew McDermott <matthewmcd07@gmail.com>
2025-01-06 13:16:05 +1300

    Removed create

--- kubejs/server_scripts/gravel_duplicate_removal.js
+++ kubejs/server_scripts/gravel_duplicate_removal.js
@@ -2,11 +2,6 @@ ServerEvents.recipes(
     event => {
         event.remove({output: 'minecraft:gravel'});
 
-        event.recipes.createCrushing(['minecraft:gravel', Item.of('minecraft:moss_block').withChance(0.1)], '#create:scrap_gravel_mossy');
-        event.recipes.createCrushing('minecraft:gravel', '#create:scrap_gravel');
-
-        event.recipes.createMilling('minecraft:gravel', 'minecraft:cobblestone');
-
         event.recipes.farmersdelight.cutting(
             'minecraft:gravel',
             '#forge:tools/shovels',
